9b78c8d6d37bccddd9267b12e5b2d16642fad0e1
ostap <guestsoft@gmail.com>
2021-07-21 20:41:44 +0300

    hooksuse

---
+++ src/profile/hookExp.jsx
@@ -0,0 +1,32 @@
+import React, {useRef, useState} from "react";
+
+
+function HooksExp(props) {
+    const [state, setState] = useState({
+        text:'Ostap',
+        id:25
+    });
+
+    let inputRef = useRef()
+
+    function setButtonClick(){
+        return setState((prev)=>{
+            return{
+                ...prev,
+                text:inputRef.current.value
+            }
+        })
+    }
+
+    return(
+            <div>
+                <input type="text" ref={inputRef}/>
+                <pre>
+                    {JSON.stringify(state,null,2)}
+                </pre>
+                <button onClick={setButtonClick}>Click</button>
+            </div>
+    )
+}
+
+export default HooksExp
