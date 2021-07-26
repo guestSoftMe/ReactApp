759328131ec729f2affa725de067b0f23fc0bf1f
guestSoftMe <73720335+guestSoftMe@users.noreply.github.com>
2021-07-26 11:33:27 +0300

    commit

--- src/profile/profileHooks.jsx
+++ src/profile/profileHooks.jsx
@@ -2,8 +2,9 @@ import React, {Suspense, useEffect} from "react";
 import {connect} from "react-redux";
 import {withRouter} from "react-router-dom";
 import {profileUserApi} from "../reduse/reduserSitebar";
-import {userthunkhooks} from "../reduse/reduserHooks";
+import {usersPhoto, userthunkhooks} from "../reduse/reduserHooks";
 import logouser from "./../600px-User_icon_3.svg.png"
+import axios from "axios";
 
 
 const HooksUse = React.lazy(() => import('./hooksuse'));
@@ -17,22 +18,35 @@ export const LazyLoading = Component => {
 
 const Hooks = (props) => {
     useEffect(() => {
-            let userId = props.match.params.userid
-            let id = props.isAuth.data.id
-            props.userthunkhooks(userId)
-            if (!userId) props.userthunkhooks(id)
+        let userId = props.match.params.userid
+        let id = props.isAuth.data.id
+        props.userthunkhooks(userId)
+        if (!userId) props.userthunkhooks(id)
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
-
 
     let onclicks = (e) => {
         let file = e.target.files[0]
-    }
+        let formData = new FormData();
+        formData.append(
+            "image",file
+        );
+        axios.post(`https://social-network.samuraijs.com/api/1.0//profile/photo`,formData,{
+            withRouter:true,
+            headers: {
+                'Content-Type': 'multipart/form-data'
+            }
 
+        })
+            .then(response=>{
+                console.log(response.data)
+            })
+    }
     return (
         <div>
             <div style={{
                 border: '2px dotted #fff',
-                width: '300px',
+                width: '400px',
                 hight: '200px',
                 margin: '20px',
             }}>
@@ -43,15 +57,15 @@ const Hooks = (props) => {
                             <div style={{margin: "10px"}}>
                                 <img style={{width: '100px'}} src={props.profile.photos.small || logouser} alt=""/>
                             </div>
-                            <div style={{fontWeight:700}}>
+                            <div>
                                 <span style={{fontSize: "25px"}}>Профиль</span>
-                                <div>Имя: {props.profile.fullName}</div>
-                                <div>Ваш id: {props.profile.userId}</div>
-                                <div>Поиск работы: {props.lookingForAJob ? 'Yes' : 'No'}</div>
+                                <div><b>Full name:</b> {props.profile.fullName}</div>
+                                <div><b>You id:</b> {props.profile.userId}</div>
+                                <div><b>Find jobs:</b> {props.lookingForAJob ? 'Yes' : 'No'}</div>
+                                <div><b>About for me:</b> {props.profile.aboutMe}</div>
                                 {props.profile.userId === props.isAuth.data.id &&
-                               <button onClick={()=>document.querySelector('#inputFile').click()}><input type="file" id={'inputFile'} onChange={onclicks} style={{
-                                    display:'none'
-                                }}/>Загрузить</button>
+                                // <input type="file" id={'inputFile'} onChange={onclicks} />
+                                <input type="file" id={'inputFile'} onChange={onclicks} />
                                 }
                             </div>
                         </div>
@@ -71,6 +85,6 @@ let mapStateToProps = (state) => {
     }
 }
 let UserMap = withRouter(Hooks)
-export default connect(mapStateToProps, {profileUserApi, userthunkhooks})(UserMap)
+export default connect(mapStateToProps, {usersPhoto,profileUserApi, userthunkhooks})(UserMap)
 
 
--- src/reduse/reduserHooks.js
+++ src/reduse/reduserHooks.js
@@ -7,12 +7,16 @@ export function Hooks(state = initialState, action) {
   switch (action.type) {
       case 'User':
         return{...state,profile:action.profile}
+      case 'Photo':
+          debugger
+        return{...state,profile:{...state.profile,photos:action.photos}}
     default:
         return state
   }
 }
 
 export let usersToo = (profile)=>({type:'User',profile})
+export let usersPhoto = (photos)=>({type:'Photo',photos})
 
 export let userthunkhooks = (userId)=>(dispatch)=>{
     axios.get(`https://social-network.samuraijs.com/api/1.0/profile/${userId}`)
@@ -20,3 +24,17 @@ export let userthunkhooks = (userId)=>(dispatch)=>{
             dispatch(usersToo(response.data))
         })
 }
+// export let userthunkphotos = (file)=>(dispatch)=>{
+//     const formData = new FormData();
+//     formData.append(
+//         "image",file
+//     );
+//     axios.put(`https://social-network.samuraijs.com/api/1.0/profile/photo`,formData)
+//         .then(response=>{
+//     debugger
+//             if (response.data.resultCode === 0) {
+//                 dispatch(usersPhoto(response.data.photos))
+//             }
+//             console.log(response)
+//         })
+// }
