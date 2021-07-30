83007ae8de90e4cc59556a1013234a82a21eec16
guestSoftMe <73720335+guestSoftMe@users.noreply.github.com>
2021-07-30 20:01:26 +0300

    put request
    (cherry picked from commit c90f078c1b2df05b63f1ed4adcae4832fd03c924)

--- src/profile/createprofile.jsx
+++ src/profile/createprofile.jsx
@@ -30,6 +30,7 @@ const Createprofile = (props) => {
                                 <img style={{width: '100px'}} src={props.profile.photos.small || logouser} alt=""/>
                             </div>
                             <div>
+
                                 <span style={{fontSize: "25px"}}>Профиль</span><span><button>Save</button></span>
                                 <div style={{margin: '10px 0'}}>
 
